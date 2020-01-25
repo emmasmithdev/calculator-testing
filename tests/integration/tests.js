9d27030aeec01d74eb5f89e412fae20e7dd05ed8
Emma <emmagray183@gmail.com>
2020-01-25 12:25:17 +0000

    UI decimal numbers test passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -63,4 +63,12 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-6')
   })
 
+  it('should show expected output for decimal numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click()
+    element(by.css('#operator_divide')).click()
+    element(by.css('#number4')).click()
+    element(by.css('#operator_equals')).click()
+    expect(running_total.getAttribute('value')).to.eventually.equal('2.25')
+  })
 });
