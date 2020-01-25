1ba47e5ea67a781199bce873ac4a92bd141c470a
Emma <emmagray183@gmail.com>
2020-01-25 12:19:42 +0000

    UI very large numbers test passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -37,4 +37,21 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('10')
   })
 
+  it('should show expected output for very large numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click()
+    element(by.css('#number9')).click()
+    element(by.css('#number9')).click()
+    element(by.css('#operator_multiply')).click()
+    element(by.css('#number9')).click()
+    element(by.css('#number9')).click()
+    element(by.css('#number9')).click()
+    element(by.css('#operator_multiply')).click()
+    element(by.css('#number9')).click()
+    element(by.css('#number9')).click()
+    element(by.css('#number9')).click()
+    element(by.css('#operator_equals')).click()
+    expect(running_total.getAttribute('value')).to.eventually.equal('997002999')
+  })
+
 });
