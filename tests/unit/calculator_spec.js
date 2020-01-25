f4f54a2d5cfb304d045701a98bbad52e88bcd6e5
Emma <emmagray183@gmail.com>
2020-01-25 11:20:28 +0000

    subtract function test passing

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -7,10 +7,16 @@ describe('calculator', function () {
   });
 
   // write unit tests here in the form of "it should do something..."
-  it('it can add 2 numbers', function(){
+  it('can add 2 numbers', function(){
     calculator.previousTotal = "1"
     calculator.add("4")
     assert.equal(5.0, calculator.runningTotal)
+  })
+
+  it('can subtract 2 numbers', function(){
+    calculator.previousTotal = "7"
+    calculator.subtract("4")
+    assert.equal(3.0, calculator.runningTotal)
   })
 
 });
